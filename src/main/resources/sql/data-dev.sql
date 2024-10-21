b426091aed23b5148f566eed28d98e761d28d1f4
fernando.varelamarti <fernando.varelamarti@plexus.es>
2024-10-21 09:46:48 +0200

    update resources and pom

---
+++ src/main/resources/sql/data-dev.sql
@@ -0,0 +1,24 @@
+INSERT INTO STARSHIPS (name, type)
+VALUES ('X-Wing', 'attack');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Millennium Falcon', 'freighter');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('TIE Fighter', 'fighter');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer0', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer1', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer2', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer3', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer4', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer5', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer6', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer7', 'capital');
+INSERT INTO STARSHIPS (name, type)
+VALUES ('Star Destroyer8', 'capital');
